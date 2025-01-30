08704cfb08f61d377f7fb53d45802e9f2c008555
Brunoarcofitnessapp <brunoarcofitness@gmail.com>
2025-01-30 20:36:24 -0300

    Update mainapi.js

--- src/api/mainapi.js
+++ src/api/mainapi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const prodapi = "https://nicomaggifitness.herokuapp.com/api";
+ const prodapi = "https://mi-proyecto-backend.onrender.com/api";
 const localapi = "http://localhost:5000/api";
 export const api = axios.create({ baseURL: prodapi });
